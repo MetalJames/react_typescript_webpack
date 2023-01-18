323da7cffe05ba2408a5c5fc51a098b2bf2b97f6
Volodymyr <volodyaruzhak@gmail.com>
2023-01-18 17:33:58 -0500

    installed and add custom prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
-import React from 'react';
-import './styles.css';
-import IMAGE from './react.png';
-import LOGO from './react-2.svg';
-import { ClickCounter } from './ClickCounter';
+import React from 'react'
+import './styles.css'
+import IMAGE from './react.png'
+import LOGO from './react-2.svg'
+import { ClickCounter } from './ClickCounter'
 
 export const App = () => {
-    return (
-        <>
-            <h1>WOW React Typescript Webpack Starter Template - {process.env.NODE_ENV}</h1>
-            {/* here we bring our variables */}
-            <h2>{process.env.name}</h2>
-            <img src={IMAGE} alt='react logo' width='300' height='200' />
-            <img src={LOGO} alt='react logo' width='300' />
-            <ClickCounter />
-        </>
-    )
+  return (
+    <>
+      <h1>
+        WOW React Typescript Webpack Starter Template - {process.env.NODE_ENV}
+      </h1>
+      {/* here we bring our variables */}
+      <h2>{process.env.name}</h2>
+      <img src={IMAGE} alt="react logo" width="300" height="200" />
+      <img src={LOGO} alt="react logo" width="300" />
+      <ClickCounter />
+    </>
+  )
 }
